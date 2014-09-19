7b17ed74aa2ae8a7401e9cb9456e802e2246bc08
Michae Lese <mikelese@gmail.com>
2014-09-19 15:13:12 -0400

    Tentative add method

--- list.h
+++ list.h
@@ -11,5 +11,16 @@ typedef struct node {
 
 /* your function declarations associated with the list */
 
+void listadd(node *head, int i) {
+
+	while(head != NULL) {
+		head = head -> next;
+	}
+
+	node n = {.val = i, .next = NULL};
+
+	head -> next = n;
+}
+
 
 #endif // __LIST_H__
